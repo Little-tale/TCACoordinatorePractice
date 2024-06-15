44f7007fe465cb8f28bfe45bb3f0e26459b03e49
Kim JaeHyung <you3192001@icloud.com>
2024-06-15 14:08:13 +0900

    Initial Commit

---
+++ TCACoordinatorePractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCACoordinatorePractice
+//
+//  Created by Jae hyung Kim on 6/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCACoordinatorePractice/TCACoordinatorePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCACoordinatorePracticeApp.swift
+//  TCACoordinatorePractice
+//
+//  Created by Jae hyung Kim on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct TCACoordinatorePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
